56c05310ab087374b68ed9dcb220d1d1be0bc4b1
Leonid Semenov <ln.sm.study@gmail.com>
2026-04-13 16:07:18 +0900

    Initial Commit

---
+++ CalloverDesktop/CalloverDesktopApp.swift
@@ -0,0 +1,21 @@
+//
+//  CalloverDesktopApp.swift
+//  CalloverDesktop
+//
+//  Created by Leonid  on 13.04.26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct CalloverDesktopApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
